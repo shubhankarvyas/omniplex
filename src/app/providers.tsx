df09be36d42aa3572a5ce50d5e94d390d303c1e1
Shubhankar vyas <148957596+shubhankarvyas@users.noreply.github.com>
2025-07-23 20:09:19 +0530

    fixing

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,8 +3,7 @@
 import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 import { NextUIProvider } from "@nextui-org/react";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "../store/store";
@@ -34,8 +33,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
           </Script>
           {children}
           <Toaster />
-          <Analytics />
-          <SpeedInsights />
+
         </NextUIProvider>
       </PersistGate>
     </Provider>
